8bcf646d92d686ce244060237869c93275db5809
Justyn Temme <justyntemme@gmail.com>
2018-06-19 09:48:24 -0500

    Remove un-needed function

--- gigasecond/gigasecond.php
+++ gigasecond/gigasecond.php
@@ -1,8 +1,4 @@
 <?php
-function datesetup($b) {
-    return strtotime("Y-m-d H:i:s",$b);
-}
-
 function from($b) {
     $GIGA_SECOND = DateInterval::createFromDateString('1000000000 seconds');
     return date_add($b, $GIGA_SECOND);
